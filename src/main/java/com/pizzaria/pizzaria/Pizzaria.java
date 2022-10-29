375c2851edc669f1f3a114331edc305de9f1aaf5
viniciuscarvalhopires <vinicpires03@gmail.com>
2022-10-29 19:53:03 -0300

    Pizza

--- src/main/java/com/pizzaria/pizzaria/Pizzaria.java
+++ src/main/java/com/pizzaria/pizzaria/Pizzaria.java
@@ -12,6 +12,6 @@ package com.pizzaria.pizzaria;
 public class Pizzaria {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello teste!");
     }
 }
